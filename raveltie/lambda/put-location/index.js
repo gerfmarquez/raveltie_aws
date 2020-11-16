023994c3c226d8dddc6f1dc9aeaab77601b5123e
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:30:04 -0500

    Update index.js

--- raveltie/lambda/put-location/index.js
+++ raveltie/lambda/put-location/index.js
@@ -1,15 +1,15 @@
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
+ */
+
 console.log('Loading function');
 
 const doc = require('dynamodb-doc');
 
 const dynamo = new doc.DynamoDB();
 
-
-/** This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- * Copyright 2020, Gerardo Marquez.
- */
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
 
