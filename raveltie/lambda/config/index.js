3ff90146902c2ef3043c07e23fb7a50ae8ff723e
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:27:55 -0500

    Update index.js

--- raveltie/lambda/config/index.js
+++ raveltie/lambda/config/index.js
@@ -1,3 +1,9 @@
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
+ */
+
 exports.handler = (event, context, callback) => {
     // TODO implement
     var config = {"killswitch":false,"minimumRequiredVersion":1};
