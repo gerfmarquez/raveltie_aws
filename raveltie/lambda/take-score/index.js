2a9e3f92c5cbcf89880184402f62d9e8e786279b
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:31:29 -0500

    Update index.js

--- raveltie/lambda/take-score/index.js
+++ raveltie/lambda/take-score/index.js
@@ -1,3 +1,10 @@
+
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
+ */
+
 console.log('Loading function');
 
 const doc = require('dynamodb-doc');
@@ -5,16 +12,6 @@ const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
 
 
-/**
- * Demonstrates a simple HTTP endpoint using API Gateway. You have full
- * access to the request and response payload, including headers and
- * status code.
- *
- * To scan a DynamoDB table, make a GET request with the TableName as a
- * query string parameter. To put, update, or delete an item, make a POST,
- * PUT, or DELETE request respectively, passing in the payload to the
- * DynamoDB API as a JSON body.
- */
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log(event)
